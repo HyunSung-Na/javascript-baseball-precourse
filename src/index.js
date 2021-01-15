15b834a619001c6e6a251e06bb950b1daaec3113
HyunSung-Na <59365947+HyunSung-Na@users.noreply.github.com>
2021-01-15 13:49:43 +0900

    feat: 게임 시작, 재시작 이벤트 리스너 추가

--- src/index.js
+++ src/index.js
@@ -1,13 +1,57 @@
+import ComputerInput from './computer_input.js';
+import Game from './game.js';
+import { VICTORY_MESSAGE } from './play_result.js';
+import Validator from './user_input_validator.js';
+
+const computerInputNumbers = new ComputerInput();
+const userInputNumbers = document.querySelector("#user-input");
+const buttonSubmit = document.querySelector("#submit");
+const gameResult = document.querySelector("#result");
+const parrent = document.querySelector("#app");
+
+const ERROR_MESSAGE = `숫자를 다시 입력해주세요.
+1~9까지의 수를 중복없이 3개를 작성해주세요.`
+
 export default function BaseballGame() {
   this.play = function (computerInputNumbers, userInputNumbers) {
-    return "결과 값 String";
+    const result = Game(computerInputNumbers, userInputNumbers);
+    gameResult.innerHTML = `${result}`;
+
+    if (result === VICTORY_MESSAGE) {
+      parrent.appendChild(createRestartBtn());
+      const restartGame = document.querySelector("#restart");
+      
+      restartGame.addEventListener(`click`, () => {
+        history.go(0);
+      });
+    }
+
+    return result;
   };
+
 }
 
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
-//     return "결과 값 String";
-//   }
-// }
+function createRestartBtn() {
+  const text = "게임을 새로 시작하시겠습니까?";
+  const restartBtn = document.createElement("div");
+  restartBtn.innerHTML = 
+  `<span>${text}</span>
+  <button id="restart">게임재시작</button>`;
+  return restartBtn;
+}
 
-new BaseballGame();
+function reload(userInput) {
+  userInput.value = '';
+  userInput.focus();
+}
+
+const baseBallGame = new BaseballGame();
+
+buttonSubmit.addEventListener('click', () => {
+  if (Validator(userInputNumbers)) {
+    alert(ERROR_MESSAGE);
+  } else {
+    baseBallGame.play(computerInputNumbers, userInputNumbers);
+  }
+  reload(userInputNumbers);
+});
